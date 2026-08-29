f0cdb73586f556ee7fa15617c24cd97133f1375c
t4rxnn-devel <tarunkar29@gmail.com>
2026-08-29 13:29:07 +0300

    Update FetchToDecode stage register structure
    Refactor FetchToDecode stage register to include stall signal and remove pc and instr.

--- pipeline_regs.ml
+++ pipeline_regs.ml
@@ -1,24 +1,23 @@
 open Hardcaml
 open Signal
 
-module FetchToDecode = struct
+module StageReg (Config : sig val width : int end) = struct
   module I = struct
     type 'a t = {
       clock : 'a;
       clear : 'a;
-      pc    : 'a[@width 32];
-      instr : 'a[@width 32];
+      stall : 'a;
+      d     : 'a[@width Config.width];
     } [@@deriving sexp_of, hardcaml]
   end
 
   module O = I
-  
+
   let create scope (i : I.t) =
     let spec = Reg_spec.create ~clock:i.clock ~clear:i.clear () in
-    {
-      O.clock = i.clock;
-      clear = i.clear;
-      pc    = Reg.spec spec i.pc;
-      instr = Reg.spec spec i.instr;
-    }
+    (* Hold current state if stall is asserted, otherwise load new data *)
+    let q = Reg.spec spec (mux2 i.stall (reg_specify spec i.d) i.d) in
+    { O.clock = i.clock; clear = i.clear; stall = i.stall; d = q }
 end
+
+(* Pipeline register boundaries can be typed and instantiated using record bundles *)
