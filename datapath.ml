aa48a8754a0efa9e4acb4e5b3647cc86b730bb1b
t4rxnn-devel <tarunkar29@gmail.com>
2026-08-29 13:14:28 +0300

    Enhance datapath.ml with comments and code organization
    Refactor datapath.ml to improve clarity and structure of the code by adding comments and reorganizing sections.

--- datapath.ml
+++ datapath.ml
@@ -20,19 +20,36 @@ end
 
 let create (scope : Scope.t) (i : I.t) =
   let open Signal in
-  (* Program Counter Register *)
-  let pc = Reg.spec (Reg_spec.create ~clock:i.clock ~clear:i.clear ()) 
-                    (width 32) in
-  let next_pc = pc + ಮಾಹಿತ (signal ~width:32 "4") in
+  
+  (* 1. Program Counter Register & Increment Logic *)
+  let pc = Reg.spec (Reg_spec.create ~clock:i.clock ~clear:i.clear ()) (width 32) in
+  let next_pc = pc +:. 4 in
   pc <== next_pc;
 
-  (* Simple Register File using RAM or explicit registers *)
-  let rs1_addr = i.instrselect:[19, 15] in
-  let rs2_addr = i.instrselect:[24, 20] in
-  let rd_addr  = i.instrselect:[11, 7] in
+  (* 2. RV32I Instruction Field Slicing *)
+  let opcode = select i.instr 6 0 in
+  let funct3 = select i.instr 14 12 in
+  let funct7 = select i.instr 31 25 in
+  let rs1    = select i.instr 19 15 in
+  let rs2    = select i.instr 24 20 in
+  let rd     = select i.instr 11 7 in
 
-  (* Instantiate ALU *)
-  let alu_input = { ALU.I.a = pc; b = i.mem_rdata; op = select i.instr 14 12 } in
+  (* 3. Control Unit Decoding *)
+  let ctrl = Control.create scope { Control.I.opcode; funct3; funct7 } in
+
+  (* 4. Hardware Register File Instantiation *)
+  let regfile_out = Regfile.create scope {
+    Regfile.I.clock;
+    we  = ctrl.reg_write;
+    wa  = rd;
+    wd  = zero 32; (* Tied to write-back bus later *)
+    ra1 = rs1;
+    ra2 = rs2;
+  } in
+
+  (* 5. ALU Data Path Integration *)
+  let alu_b = mux2 ctrl.alu_src (zero 32) regfile_out.rd2 in
+  let alu_input = { ALU.I.a = regfile_out.rd1; b = alu_b; op = ctrl.alu_op } in
   let alu_res = ALU.create scope alu_input in
 
-  { O.pc; alu_out = alu_res.result; mem_wdata = zero 32 }
+  { O.pc; alu_out = alu_res.result; mem_wdata = regfile_out.rd2 }
